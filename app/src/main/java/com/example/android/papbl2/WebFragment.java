c873e40538749e8ced38bb4534b742471dc7f430
Muhammad Budi fauzan <budifauzan1997@gmail.com>
2019-02-24 14:19:32 +0700

    Can take the json from the web in String form
    Used the teacher repository

--- app/src/main/java/com/example/android/papbl2/WebFragment.java
+++ app/src/main/java/com/example/android/papbl2/WebFragment.java
@@ -1,7 +1,12 @@
 package com.example.android.papbl2;
 
+import android.app.ProgressDialog;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,7 +16,13 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class WebFragment extends Fragment {
@@ -21,6 +32,9 @@ public class WebFragment extends Fragment {
     EditText searchText;
     Button search;
     ArrayList<ExchangeRates> rates;
+    ProgressDialog progressDialog;
+    String webContent;
+    HttpURLConnection httpConn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -48,32 +62,142 @@ public class WebFragment extends Fragment {
                 final TextView arrow = view.findViewById(R.id.web_list_item_arrow);
                 final TextView rate = view.findViewById(R.id.web_list_item_rate);
                 double exchangeRate = rates.get(position).getRate();
-                if (arrow.getText().toString().equalsIgnoreCase("->")) {
-                    arrow.setText("<-");
+                if (arrow.getText().toString().equalsIgnoreCase("→")) {
+                    arrow.setText("←");
                     exchangeRate = 1 / exchangeRate;
                     rate.setText(String.format("%.2f", exchangeRate));
                 } else {
-                    arrow.setText("->");
+                    arrow.setText("→");
                     rate.setText(String.format("%.10f", exchangeRate));
                 }
             }
         });
 
         ArrayList<ExchangeRates> resultRates = new ArrayList<>();
+
         search.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                for (int i = 0; i < rates.size(); i++) {
-                    if (rates.get(i).getCurrency().startsWith(searchText.getText().toString().toUpperCase())) {
-                        Toast.makeText(rootView.getContext(), rates.get(i).getCurrency(), Toast.LENGTH_SHORT).show();
-                    }
-                }
+//                for (int i = 0; i < rates.size(); i++) {
+//                    if (rates.get(i).getCurrency().startsWith(searchText.getText().toString().toUpperCase())) {
+//                        Toast.makeText(rootView.getContext(), rates.get(i).getCurrency(), Toast.LENGTH_SHORT).show();
+//                    }
+//                }
+                downloadWebContent("https://api.exchangeratesapi.io/latest?base=IDR");
             }
         });
 
 
         return rootView;
+    }
+
+    private void downloadWebContent(String urlStr) {
+
+        ConnectivityManager connMgr = (ConnectivityManager) rootView.getContext().getSystemService(rootView.getContext().CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        // Check for network connections
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+            // Create background thread to connect and get data
+
+            new DownloadWebTask().execute(urlStr);
+        } else {
+            Toast.makeText(rootView.getContext(), "No network connection available.", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+    public class DownloadWebTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog = new ProgressDialog(rootView.getContext());
+            progressDialog.setMessage("Please Wait");
+            progressDialog.show();
+        }
+
+
+        @Override
+        protected String doInBackground(String... params) {
+
+
+            String url = params[0];
+            InputStream in;
+
+            try {
+                StringBuilder sb = new StringBuilder();
+                in = openHttpConnection(url);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+
+                String nextLine;
+                while ((nextLine = reader.readLine()) != null) {
+                    sb.append(nextLine);
+                    webContent = sb.toString();
+                    in.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.e("Error", "doInBackground: Input stream process");
+            }
+
+            return webContent;
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            Toast.makeText(rootView.getContext(), result, Toast.LENGTH_SHORT).show();
+            httpConn.disconnect();
+            progressDialog.dismiss();
+
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+
+
+        private InputStream openHttpConnection(String urlStr) {
+            InputStream in = null;
+            int resCode = -1;
+
+            try {
+                URL url = new URL(urlStr);
+
+                httpConn = (HttpURLConnection) url.openConnection();
+                httpConn.setAllowUserInteraction(false);
+                httpConn.setInstanceFollowRedirects(true);
+                httpConn.setReadTimeout(10000 /* milliseconds */);
+                httpConn.setConnectTimeout(15000 /* milliseconds */);
+                httpConn.setRequestMethod("GET");
+                httpConn.connect();
+                resCode = httpConn.getResponseCode();
+
+                if (resCode == HttpURLConnection.HTTP_OK) {
+                    in = httpConn.getInputStream();
+
+                } else {
+                    Log.e("HTTP", "openHttpConnection: Host not reached");
+                }
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                Log.e("Error", "openHttpConnection: URL not correct", e);
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.e("Error", "openHttpConnection: host not reach", e);
+            }
+            return in;
+        }
+
+
     }
 
 }
